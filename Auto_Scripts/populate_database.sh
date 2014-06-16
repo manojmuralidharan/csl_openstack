0b0ec0cf790782d8ece12bbdd045448c044dade6
root <root@calchfc167.(none)>
2014-06-16 17:51:23 +0530

    modified

--- Auto_Scripts/populate_database.sh
+++ Auto_Scripts/populate_database.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-mysql -uroot -p$OPENSTACK_PW << EOF
+mysql -uroot -popenstackcs << EOF
 CREATE DATABASE keystone;
-GRANT ALL ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$OPENSTACK_PW';
-GRANT ALL ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '$OPENSTACK_PW';
+GRANT ALL ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'openstackcs';
+GRANT ALL ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'openstackcs';
 
 CREATE DATABASE glance;
-GRANT ALL ON glance.* TO 'glance'@'%' IDENTIFIED BY '$OPENSTACK_PW';
-GRANT ALL ON glance.* TO 'glance'@'localhost' IDENTIFIED BY '$OPENSTACK_PW';
+GRANT ALL ON glance.* TO 'glance'@'%' IDENTIFIED BY 'openstackcs';
+GRANT ALL ON glance.* TO 'glance'@'localhost' IDENTIFIED BY 'openstackcs';
 
 CREATE DATABASE neutron;
-GRANT ALL ON neutron.* TO 'neutron'@'%' IDENTIFIED BY '$OPENSTACK_PW';
-GRANT ALL ON neutron.* TO 'neutron'@'localhost' IDENTIFIED BY '$OPENSTACK_PW';
+GRANT ALL ON neutron.* TO 'neutron'@'%' IDENTIFIED BY 'openstackcs';
+GRANT ALL ON neutron.* TO 'neutron'@'localhost' IDENTIFIED BY 'openstackcs';
 
 CREATE DATABASE nova;
-GRANT ALL ON nova.* TO 'nova'@'%' IDENTIFIED BY '$OPENSTACK_PW';
-GRANT ALL ON nova.* TO 'nova'@'localhost' IDENTIFIED BY '$OPENSTACK_PW';
+GRANT ALL ON nova.* TO 'nova'@'%' IDENTIFIED BY 'openstackcs';
+GRANT ALL ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'openstackcs';
 
 CREATE DATABASE cinder;
-GRANT ALL ON cinder.* TO 'cinder'@'%' IDENTIFIED BY '$OPENSTACK_PW';
-GRANT ALL ON cinder.* TO 'cinder'@'localhost' IDENTIFIED BY '$OPENSTACK_PW';
+GRANT ALL ON cinder.* TO 'cinder'@'%' IDENTIFIED BY 'openstackcs';
+GRANT ALL ON cinder.* TO 'cinder'@'localhost' IDENTIFIED BY 'openstackcs';
 
 CREATE DATABASE heat;
-GRANT ALL ON heat.* TO 'heat'@'%' IDENTIFIED BY '$OPENSTACK_PW';
-GRANT ALL ON heat.* TO 'heat'@'localhost' IDENTIFIED BY '$OPENSTACK_PW';
+GRANT ALL ON heat.* TO 'heat'@'%' IDENTIFIED BY 'openstackcs';
+GRANT ALL ON heat.* TO 'heat'@'localhost' IDENTIFIED BY 'openstackcs';
 EOF
